bf3e41c880ff165f08b3d61a6f742cb5d2786b81
Gabriel Machado <g_machado1@hotmail.com>
2018-03-23 13:17:32 -0300

    Implementado validações no cadastro de produtos

--- src/main/java/br/com/casadocodigo/loja/controllers/ProdutoController.java
+++ src/main/java/br/com/casadocodigo/loja/controllers/ProdutoController.java
@@ -1,8 +1,13 @@
 package br.com.casadocodigo.loja.controllers;
 
+import javax.validation.Valid;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.WebDataBinder;
+import org.springframework.web.bind.annotation.InitBinder;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
@@ -10,6 +15,7 @@ import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 import br.com.casadocodigo.loja.daos.ProdutoDAO;
 import br.com.casadocodigo.loja.models.Produto;
 import br.com.casadocodigo.loja.models.TipoPreco;
+import br.com.casadocodigo.loja.validation.ProdutoValidation;
 
 @Controller
 @RequestMapping("produtos")
@@ -17,6 +23,11 @@ public class ProdutoController {
 	
 	@Autowired
     private ProdutoDAO produtoDao;
+	
+	@InitBinder
+	public void InitBinder(WebDataBinder binder) {
+	        binder.addValidators(new ProdutoValidation());
+	}
 
 	@RequestMapping("/form")
 	public String form(Model model) {
@@ -25,8 +36,12 @@ public class ProdutoController {
 	}
 	
 	@RequestMapping(method=RequestMethod.POST)
-	public String gravar(Produto produto, RedirectAttributes redirectAttributes) {
-		System.out.println(produto);
+	public String gravar(@Valid Produto produto, BindingResult result, RedirectAttributes redirectAttributes, Model model) {
+
+		if (result.hasErrors()) {
+            return form(model);
+        }
+		
 		produtoDao.gravar(produto);
 		redirectAttributes.addFlashAttribute("sucesso", "Produto cadastrado com sucesso!");
 		return "redirect:produtos";
