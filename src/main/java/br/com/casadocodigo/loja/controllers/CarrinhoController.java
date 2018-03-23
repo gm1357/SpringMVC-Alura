33dcf06b0984999d86f16146309899d7c488658f
Gabriel Machado <g_machado1@hotmail.com>
2018-03-23 17:58:33 -0300

    Implementado carrinho de compras e formatado corretamente a data

---
+++ src/main/java/br/com/casadocodigo/loja/controllers/CarrinhoController.java
@@ -0,0 +1,35 @@
+package br.com.casadocodigo.loja.controllers;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import br.com.casadocodigo.loja.daos.ProdutoDAO;
+import br.com.casadocodigo.loja.models.CarrinhoCompras;
+import br.com.casadocodigo.loja.models.CarrinhoItem;
+import br.com.casadocodigo.loja.models.Produto;
+import br.com.casadocodigo.loja.models.TipoPreco;
+
+@Controller
+@RequestMapping("/carrinho")
+public class CarrinhoController {
+
+	@Autowired
+	private ProdutoDAO produtoDao;
+	@Autowired
+    private CarrinhoCompras carrinho;
+
+	private CarrinhoItem criaItem(Integer produtoId, TipoPreco tipo){
+	    Produto produto = produtoDao.find(produtoId);
+	    CarrinhoItem carrinhoItem = new CarrinhoItem(tipo, produto);
+	    return carrinhoItem;
+	}
+	
+	@RequestMapping("add")
+	public String add(Model model, Integer produtoId, TipoPreco tipo) {
+		CarrinhoItem carrinhoItem = criaItem(produtoId, tipo);
+		carrinho.add(carrinhoItem);
+		return "redirect:/produtos";
+	}
+}
