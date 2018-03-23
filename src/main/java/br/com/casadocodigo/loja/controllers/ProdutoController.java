9fef933fa88b55dd71066712dac9d93f9922208f
Gabriel Machado <g_machado1@hotmail.com>
2018-03-23 10:40:28 -0300

    Implementado página de listagem de produtos e feito algumas correções

--- src/main/java/br/com/casadocodigo/loja/controllers/ProdutoController.java
+++ src/main/java/br/com/casadocodigo/loja/controllers/ProdutoController.java
@@ -4,29 +4,36 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 
 import br.com.casadocodigo.loja.daos.ProdutoDAO;
 import br.com.casadocodigo.loja.models.Produto;
 import br.com.casadocodigo.loja.models.TipoPreco;
 
 @Controller
+@RequestMapping("produtos")
 public class ProdutoController {
 	
 	@Autowired
     private ProdutoDAO produtoDao;
 
-	@RequestMapping("produtos/form")
-	public String form() {
+	@RequestMapping("/form")
 	public String form(Model model) {
 		model.addAttribute("tipos", TipoPreco.values());
 		return "produtos/form";
 	}
 	
-	@RequestMapping("produtos")
+	@RequestMapping(method=RequestMethod.POST)
 	public String gravar(Produto produto) {
 		System.out.println(produto);
 		produtoDao.gravar(produto);
 		return "produtos/ok";
 		
+	}
+	
+	@RequestMapping(method=RequestMethod.GET)
+	public String listar(Model model) {
+		model.addAttribute("produtos", produtoDao.listar());
+		return "produtos/lista";
 	}
 }
--- src/main/webapp/WEB-INF/views/produtos/form.jsp
+++ src/main/webapp/WEB-INF/views/produtos/form.jsp
@@ -1,11 +1,12 @@
-<%@ page language="java" contentType="text/html; charset=ISO-8859-1"
-    pageEncoding="ISO-8859-1"%>
-<!DOCTYPE html PUBLIC "-//W3C//DTD HTML 4.01 Transitional//EN" "http://www.w3.org/TR/html4/loose.dtd">
+<%@ page language="java" contentType="text/html; charset=UTF-8"
+    pageEncoding="UTF-8"%>
+<%@ taglib uri="http://java.sun.com/jsp/jstl/core" prefix="c"%>
+<!DOCTYPE>
 <html>
 <head>
-<meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+<meta charset="UTF-8">
 <title>Livros de Java, Android, iPhone, Ruby, PHP e muito mais -
-	Casa do C�digo</title>
+	Casa do Código</title>
 </head>
 <body>
 	<form action="/casadocodigo/produtos" method="POST">
@@ -14,11 +15,11 @@
 			 <input type="text" name="titulo">
 		</div>
 		<div>
-			<label>Descri�ao</label>
+			<label>Descriçao</label>
 			 <textarea name="descricao" rows="10" cols="10"></textarea>
 		</div>
 		<div>
-			<label>P�ginas</label>
+			<label>Páginas</label>
 			 <input type="number" name="paginas">
 		</div>
 		
