c6584947a72797bd5642d735096b19b9d4154b4c
Gabriel Machado <g_machado1@hotmail.com>
2018-03-28 10:23:46 -0300

    Implementado pagina para tratar erros

--- src/main/java/br/com/casadocodigo/loja/controllers/ProdutoController.java
+++ src/main/java/br/com/casadocodigo/loja/controllers/ProdutoController.java
@@ -1,5 +1,6 @@
 package br.com.casadocodigo.loja.controllers;
 
+import javax.persistence.NoResultException;
 import javax.validation.Valid;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -8,6 +9,7 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.WebDataBinder;
+import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.InitBinder;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -72,5 +74,10 @@ public class ProdutoController {
 	  model.addAttribute("produto",produto);
 	  model.addAttribute("contextPath","casadocodigo/");
 	  return "produtos/detalhes";
+	}
+	
+	@ExceptionHandler(NoResultException.class)
+	public String trataDetalheNaoEcontrado(){
+	    return "error";
 	}
 }
